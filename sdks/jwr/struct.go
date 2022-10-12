acc5311d2e8dd90760db76d5fb38e6dc892dff89
sahil <sahil.kukkar@jungleegames.com>
2022-10-12 13:53:54 +0530

    sdk changes

--- sdks/jwr/struct.go
+++ sdks/jwr/struct.go
@@ -1,5 +1,11 @@
 package jwr
 
+import (
+	"log"
+	"strings"
+	"time"
+)
+
 type UserProfile struct {
 	FirstName  string `json:"firstName"`
 	MiddleName string `json:"middleName"`
@@ -17,4 +23,26 @@ type JWRSDKConfig struct {
 	BaseURL    string
 	Token      string
 	APITimeout int
+}
+
+func (u UserProfile) GetFullName() string {
+	if strings.ToLower(u.FirstName) == "fnu" {
+		u.FirstName = ""
+	}
+	if strings.ToLower(u.LastName) == "lnu" {
+		u.LastName = ""
+	}
+	if u.MiddleName == "" {
+		return strings.TrimSpace(u.FirstName + " " + u.LastName)
+	}
+	return strings.TrimSpace(u.FirstName + " " + u.MiddleName + " " + u.LastName)
+}
+
+func (u UserProfile) GetDOB(log *log.Logger, traceID string) string {
+	t := time.Unix(0, int64(u.DOB)*int64(time.Millisecond))
+	loc, _ := time.LoadLocation("Asia/Kolkata")
+	t = t.In(loc)
+	s := t.Format("02/01/2006")
+	log.Printf("GETDOB %s : profileDOB: %d (%s) : output DOB: %s\n", traceID, u.DOB, t.Format(time.RFC3339), s)
+	return s
 }
